28d89ae64b2c1324747e7f14f724cc10e0ff5009
Tony NGUEREZA <nguerezatony@gmail.com>
2026-08-15 10:06:09 +0100

    fix: declare nullable callable in array first helper

--- src/Helper/Arr.php
+++ src/Helper/Arr.php
@@ -950,14 +950,14 @@ class Arr
     /**
      * Return the first element in an array passing a given truth test.
      * @param array<mixed> $array
-     * @param callable $callable
+     * @param callable|null $callable
      * @param mixed $default
      *
      * @return mixed
      */
     public static function first(
         array $array,
-        callable $callable = null,
+        ?callable $callable = null,
         $default = null
     ) {
         if ($callable === null) {
