38bfc254de2034282bc93720847723e0e3c2aaf6
Tony NGUEREZA <nguerezatony@gmail.com>
2026-08-18 16:30:14 +0100

    feat(Arr): Improve offsetGet and offsetSet to accept mixed types

--- tests/Helper/ArrTest.php
+++ tests/Helper/ArrTest.php
@@ -854,22 +854,22 @@ class ArrTest extends PlatineTestCase
         $aa =  new class implements ArrayAccess{
             private $arr = [1, 4, 5];
 
-            public function offsetExists($offset): bool
+            public function offsetExists(mixed $offset): bool
             {
                 return isset($this->arr[$offset]);
             }
 
-            public function offsetGet($offset)
+            public function offsetGet(mixed $offset): mixed
             {
                 return $this->arr[$offset];
             }
 
-            public function offsetSet($offset, $value): void
+            public function offsetSet(mixed $offset, mixed $value): void
             {
                 $this->arr[$offset] = $value;
             }
 
-            public function offsetUnset($offset): void
+            public function offsetUnset(mixed $offset): void
             {
                 unset($this->arr[$offset]);
             }
